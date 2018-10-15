0b3335b61f143841c923c124871a8720adb55761
Rui Miguel <ravage@fragmentized.net>
2018-10-16 00:15:46 +0100

    Make standardjs happy

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,26 +5,26 @@ module.exports = {
   usage: '[help]',
   cooldown: 5,
   execute (message, args) {
-    const fs = require('fs');
+    const fs = require('fs')
 
     // Read from file
-    fs.readFile('commands.json','utf8', function (err, data) {
-      if (err) throw err;
-      
+    fs.readFile('commands.json', 'utf8', function (err, data) {
+      if (err) throw err
+
       // Parse file information
-      const jsonFile =  JSON.parse(data);
-      
+      const jsonFile = JSON.parse(data)
+
       // New Array
-      let commandList = [];
+      let commandList = []
 
       /*  Fill array with json commands
         {commands: ['a', 'b']}
       */
-      commandList = jsonFile.commands;
+      commandList = jsonFile.commands
 
       // Output info
-      message.channel.send('Discord Commands');
-      message.channel.send(commandList.join(" "));
-    });
+      message.channel.send('Discord Commands')
+      message.channel.send(commandList.join(' '))
+    })
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,66 +1,55 @@
 /* File that contains classified information that is usefull
 for the the performance of this software. */
-require('dotenv').load();
+require('dotenv').load()
 
-//#region Variables 
-const path = require('path');
-const utils = require('./libs/utils.js');
-const Discord = require('discord.js');
-const client = new Discord.Client();
+// Variables
+const path = require('path')
+const utils = require('./libs/utils.js')
+const Discord = require('discord.js')
 
-const commandsDir = path.join(__dirname, 'commands');
+const client = new Discord.Client()
+const commandsDir = path.join(__dirname, 'commands')
+const botChannel = process.env.BOT_CHANNEL
+const prefix = process.env.PREFIX
 
-const botChannel = process.env.BOT_CHANNEL;
-const prefix = process.env.PREFIX;
-// #endregion
-
-//#region Methods
+// Methods
 client.on('ready', () => {
-	client.commands = utils.loadCommands(commandsDir);
-	console.log("Ready " + client.user.tag);
+  client.commands = utils.loadCommands(commandsDir)
+  console.log(`Ready ${client.user.tag}`)
 })
+
 // Event
 // Reads every message on discord
-client.on('message', message => 
-{	
-	// 
-	if (message.content.startsWith(prefix) && !message.author.bot)
-	{
-		if(message.channel.name != botChannel)
-		{
-			message.delete();
-			message.author.send('Please use the correct channel for bot commands. (#botcmd)');
-		}
-		else
-		{
-			const args = message.content.slice(prefix.length).split(/ + /);
-			const commandName = args.shift().toLowerCase();
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) { return }
 
-			const command = client.commands.get(commandName) || utils.noopCommand;
+  if (message.channel.type === 'text' && message.channel.name !== botChannel) {
+    message.delete()
+    message.author.send(`Please use the correct channel for bot commands. (#${botChannel})`)
+  } else {
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const commandName = args.shift().toLowerCase()
+    const command = client.commands.get(commandName) || utils.noopCommand
 
-			command.execute(message, args);
-		}
-	}
-});
+    command.execute(message, args)
+  }
+})
+
 // Event
 // Triggers on member joining discord
-client.on('guildMemberAdd', member => {	
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === process.env.WELCOME_CHANNEL)
 
-	const channel = member.guild.channels.find(ch => ch.name === process.env.WELCOME_CHANNEL);
+  if (!channel) { return }
 
-	if (!channel) return;
+  channel.send(`Welcome to this discord ${member}`)
+  member.send('recebeu isto? say fuckin yay on chat')
+})
 
-	channel.send('Welcome to this discord ' + member);
-
-	member.send('recebeu isto? say fuckin yay on chat');
-  });
 // Event
 // Triggers on member leaving discord
 client.on('guildMemberRemove', member => {
-	
-	member.send('we didnt like you any way.. ');
-	
-});
-// #endregion
+  member.send('we didnt like you any way.. ')
+})
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN)
